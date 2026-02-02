18f1071b04e279c9e6a5473abe8f46bf84c7065f
luishibosca01 <l.lus@outlook.es>
2026-02-02 17:12:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker para DeltaF
 // Versión del cache - incrementa este número cuando hagas cambios
-const CACHE_VERSION = 'v0.18-cache';
+const CACHE_VERSION = 'v0.21-cache';
 const CACHE_NAME = `deltaf-cache-${CACHE_VERSION}`;
 
 // Archivos a cachear
